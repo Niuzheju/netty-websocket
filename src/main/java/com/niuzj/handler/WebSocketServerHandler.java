ef30bad878c94a562a549dbc8d50304b181d4c75
niuzj <2549218666@qq.com>
2019-01-09 11:38:27 +0800

    ok

--- src/main/java/com/niuzj/handler/WebSocketServerHandler.java
+++ src/main/java/com/niuzj/handler/WebSocketServerHandler.java
@@ -2,26 +2,32 @@ package com.niuzj.handler;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.websocketx.*;
 import io.netty.util.CharsetUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedHashSet;
 import java.util.TreeSet;
 
 public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
 
     private WebSocketServerHandshaker webSocketServerHandshaker;
 
-    private TreeSet<WebSocketServerHandshaker> handshakers = new TreeSet<>();
+    private static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        channelGroup.add(ctx.channel());
+    }
 
     @Override
     protected void messageReceived(ChannelHandlerContext channelHandlerContext, Object msg) throws Exception {
@@ -52,13 +58,17 @@ public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> 
             ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
             return;
         }
-        if (!(frame instanceof TextWebSocketFrame)) {
-            throw new UnsupportedOperationException(String.format("%s frame types not supported", frame.getClass().getName()));
+//        if (!(frame instanceof TextWebSocketFrame)) {
+//            throw new UnsupportedOperationException(String.format("%s frame types not supported", frame.getClass().getName()));
+//        }
+        if (frame instanceof TextWebSocketFrame){
+            String request = ((TextWebSocketFrame) frame).text();
+            System.out.println("received " + request);
+            channelGroup.writeAndFlush(new TextWebSocketFrame(request));
+        } else if (frame instanceof BinaryWebSocketFrame){
+            ByteBuf content = frame.content();
+            channelGroup.writeAndFlush(new BinaryWebSocketFrame(content));
         }
-        String request = ((TextWebSocketFrame) frame).text();
-        System.out.println("received " + request);
-        ctx.channel().write(new TextWebSocketFrame("欢迎使用netty websocket服务， 现在时刻" + new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date())));
-
     }
 
     private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
@@ -66,14 +76,13 @@ public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> 
             sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
             return;
         }
-        WebSocketServerHandshakerFactory factory = new WebSocketServerHandshakerFactory("ws://localhost://websocket", null, false);
+        WebSocketServerHandshakerFactory factory = new WebSocketServerHandshakerFactory("ws://localhost:8084//websocket", null, false);
         webSocketServerHandshaker = factory.newHandshaker(req);
         if (webSocketServerHandshaker == null) {
             WebSocketServerHandshakerFactory.sendUnsupportedWebSocketVersionResponse(ctx.channel());
             return;
 
         }
-        handshakers.add(webSocketServerHandshaker);
         webSocketServerHandshaker.handshake(ctx.channel(), req);
     }
 
